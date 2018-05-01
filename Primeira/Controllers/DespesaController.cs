b7f57321981a36a65265c80019c4422aa1d5cb8a
Jorge Nuno Rosa Vinhas <160323007@estudantes.ips.pt>
2018-05-01 22:48:56 +0100

    Add project files.

---
+++ Primeira/Controllers/DespesaController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Primeira.Models;
+
+namespace Primeira.Controllers
+{
+    public class DespesaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult NovaDespesa()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NovaDespesa(Despesa d)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("DespesaConfirmada", d);
+            }
+            else
+                return View();
+
+        }
+    }
+}
---
+++ Primeira/Models/Despesa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Primeira.Models
+{
+    public class Despesa
+    {
+        [Required(ErrorMessage="Por favor preencha o Nome da Despesa")]
+        public string NomeDespesa { get; set; }
+        [Required(ErrorMessage = "Por favor preencha a Quantidade da Despesa")]
+        public int Quantidade { get; set; }
+        [Required(ErrorMessage = "Por favor preencha o Valor da Despesa")]
+        public double Valor { get; set; }
+        [Required(ErrorMessage = "Por fvor preencha a Categoria da Despesa")]
+        public string Categoria { get; set; }
+        [Required(ErrorMessage = "Por favor diga se está a despesa está paga ou não")]
+        public bool Pago { get; set; }
+
+
+
+    }
+}
